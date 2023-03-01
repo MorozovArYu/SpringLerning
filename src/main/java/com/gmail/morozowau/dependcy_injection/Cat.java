2fe86c8e78d9ae845bf87f4f3c5d5093925e83b1
Artem <morozowau@gmail.com>
2023-03-01 16:36:07 +0600

    Lesson 2 DI with Constructor
    In file applicationContext and Test1 dependcy_injection package there are examples of creatig DI with constructor

--- src/main/java/com/gmail/morozowau/dependcy_injection/Cat.java
+++ src/main/java/com/gmail/morozowau/dependcy_injection/Cat.java
@@ -3,6 +3,9 @@ package com.gmail.morozowau.dependcy_injection;
 import com.gmail.morozowau.dependcy_injection.Pet;
 
 public class Cat implements Pet {
+    public Cat() {
+        System.out.println("Cat bean is created"); // Diagnostic
+    }
 
     @Override
     public void say(){
--- src/main/java/com/gmail/morozowau/dependcy_injection/Dog.java
+++ src/main/java/com/gmail/morozowau/dependcy_injection/Dog.java
@@ -3,6 +3,9 @@ package com.gmail.morozowau.dependcy_injection;
 import com.gmail.morozowau.dependcy_injection.Pet;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println("Dog bean is created"); // Diagnostic
+    }
 
     @Override
     public void say(){
